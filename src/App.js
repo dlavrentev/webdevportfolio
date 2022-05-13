6c268900779684e3f694e5869daf565c29166322
dlavrentev <dimitrilavrentev96@gmail.com>
2022-05-13 14:06:25 +0200

    installed react and created welcome page

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Welcome from './Components/Welcome/Welcome';
+
+function App() {
+  return (
+    <BrowserRouter>
+    <Routes>
+    
+    <Route path="/"   element={<Welcome />}/>
+    
+    </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/Components/Welcome/Welcome.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import logo from './../../dllogo2.png';
+
+export default function Welcome() {
+  return (
+    <div className="welcome">
+      <header className="welcomepage">
+        <img src={logo} className="welcome-logo" alt="logo" />
+      </header>
+    </div>
+  )
+}
