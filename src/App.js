68b860ec09b30d7aa00b01995bb45e9558acb644
dlavrentev <dimitrilavrentev96@gmail.com>
2022-05-17 17:03:30 +0200

    added introduction

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Welcome from './Components/Welcome/Welcome';
 import Header from './Components/Header/Header';
+import Introduction from './Components/Main/Introduction';
 
 function App() {
   return (
     <BrowserRouter>
     <Header />
+    <Introduction />
     <Routes>
     
     
---
+++ src/Components/Main/Introduction.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Introduction() {
+  return (
+    <div className='introduction-container'>
+        <h2 className='introduction-hay'>Hey! My name is</h2>
+        <h1 className='introduction-name'>Dimitri Lavrentev.</h1>
+        <h1 className='introduction-welcome'>Welcome to my web development portfolio.</h1>
+        <h3 className='introduction-description'>I’m a web developer in making. At the moment i make a course at Mimo Dev. I learn a lot of things there and it’s my first step into my journey to become a coder. I hope you have little bit fun on my site. Enjoy your stay! </h3>
+        <button class="button">MIMO
+        </button>
+    </div>
+  )
+}
