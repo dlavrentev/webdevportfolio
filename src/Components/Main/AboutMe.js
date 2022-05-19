d87e991fc96483b920cff2d7b727e64499632b20
dlavrentev <dimitrilavrentev96@gmail.com>
2022-05-19 14:23:34 +0200

    before changing to 1 image

--- src/Components/Main/AboutMe.js
+++ src/Components/Main/AboutMe.js
@@ -12,9 +12,30 @@ export default function AboutMe() {
     <div className='line'></div>
     </div>
     
+    <div className='aboutme-sec'>
+    
+    <div className='aboutme-sec-left'>
+    <img className='profilepic' src={ProfilePic} height="80%"
+    width="80%"></img>
+    <p>Thank you for visiting my site! I'm Dimitri and I'm 26 years old, living in currently in Germany. My passion is to create stuff on the computer
+        and last year I started my journey to become a Full-Stack Developer. My intention for building this site, was to share my projects with the world. 
+    </p>
+    </div>
 
-    <img src={ProfilePic} height="300px"
-    width="300px"></img>
+    <div className='aboutme-sec-right'>
+    <img className='profilepic' src={ProfilePic} height="80%"
+    width="80%"></img>
+    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+    </div>
+
+    <div className='aboutme-sec-left'>
+    <img className='profilepic' src={ProfilePic} height="80%"
+    width="80%"></img>
+    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+    </div>
+    
+    </div>
+   
     
     
     </div>
